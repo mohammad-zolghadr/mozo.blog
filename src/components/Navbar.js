a70d61e66cee10626193948f9118dc58e95e06ff
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-01 11:41:40 +0330

    Responsive menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import style from "../sass/Navbar.scss";
 import gmailIcon from "../assets/images/gmail-icon.png";
+import arrowIcon from "../assets/images/arrowMenu.png";
 
 const Navbar = () => {
+  const [navTopResponsive, setNavTopResponsive] = useState(true);
+
+  const navHandlerResponsive = () => {
+    setNavTopResponsive(!navTopResponsive);
+  };
+
+  const closeNavResponsive = () => {
+    setNavTopResponsive(true);
+  };
+
+  const signIn = () => {
+    closeNavResponsive();
+  };
+
   return (
     <div>
       <header>
-        <Link to="/">
+        <Link to="/" onClick={closeNavResponsive}>
           <h1>مود بلاگ</h1>
         </Link>
 
-        <nav>
+        <div onClick={navHandlerResponsive} className="btnResponsiveMenu">
+          <span>دسترسی سریع</span>
+          <img src={arrowIcon} />
+        </div>
+
+        <nav
+          style={{
+            top: !navTopResponsive && "54px",
+          }}
+        >
           <ul>
             <li>
-              <Link to="/new-post">نوشتن پست جدید</Link>
+              <Link to="/new-post" onClick={closeNavResponsive}>
+                نوشتن پست جدید
+              </Link>
             </li>
             <li>
-              <Link to="/blog">بلاگ ها</Link>
+              <Link to="/blog" onClick={closeNavResponsive}>
+                بلاگ ها
+              </Link>
             </li>
             <li>
-              <Link to="/about-me">درباره من</Link>
+              <Link to="/about-me" onClick={closeNavResponsive}>
+                درباره من
+              </Link>
             </li>
             <li>
-              <button>
+              <button onClick={signIn}>
                 <img src={gmailIcon} />
                 ورود با Gmail
               </button>

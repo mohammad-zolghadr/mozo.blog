ce37002eacabe804f8186a9de5614c27eac59c11
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-02 00:13:59 +0330

    New Post Component Created

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,7 +1,32 @@
 import React from "react";
 
+import style from "../sass/NewPost.scss";
+import voiceIco from "../assets/images/voice-ico.png";
+
+// Function
+import { textKey, getText } from "../Text";
+
 const NewPost = () => {
-  return <div className="cContainer">New Post</div>;
+  const key = new textKey();
+  return (
+    <div className="cContainer">
+      <form className="newPostFormContainer">
+        <input type="text" placeholder={getText(key.NP_PH_Title)} />
+        <div>
+          <textarea placeholder={getText(key.NP_PH_Text)}></textarea>
+          <img src={voiceIco} />
+        </div>
+        <div className="newPostFileChooser">
+          <label htmlFor="file-upload" className="newPostFileUpload">
+            {getText(key.NP_IN_File)}
+          </label>
+          <input id="file-upload" type="file" />
+          <button>{getText(key.NP_Btn_Login)}</button>
+        </div>
+        <button className="newPostSubmit">{getText(key.NP_Btn_Post)}</button>
+      </form>
+    </div>
+  );
 };
 
 export default NewPost;

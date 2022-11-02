ec20bad4a87c47c0fb34e05ce0751e80b42d473c
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-02 14:16:48 +0330

    loading component (can cusomize)

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import style from "../sass/Loading.scss";
+
+const Loading = () => {
+  return (
+    <div className="loadingCircleContainer">
+      <div className="loadingCircle"></div>
+    </div>
+  );
+};
+
+export default Loading;

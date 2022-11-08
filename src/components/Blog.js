688055ea64e5bf58565f60d69c1689f1876944de
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2022-11-08 14:11:02 +0330

    set pagination for ALL_MOOD in Blog and Home

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 
+import style from "../sass/Blog.scss";
+
 // components
 import Post from "./Post";
 import Loading from "./Loading";
 import MoodsList from "./MoodsList";
 
 // Function
-import { getPostsList } from "../requests";
+import { getPostsList, getPostsCount } from "../requests";
 import { getText, TextKey } from "../Text";
 
 import listEmpty from "../assets/images/search_empty.png";
@@ -14,20 +16,35 @@ import listEmpty from "../assets/images/search_empty.png";
 const Blog = () => {
   const key = new TextKey();
   const [mood, setMood] = useState();
-  const [postsList, setPostsList] = useState();
-  const [postsListUnchange, setPostsListUnchange] = useState();
+  const [postsList, setPostsList] = useState([]);
+  const [postsListUnchange, setPostsListUnchange] = useState([]);
+  const [lastPostFetched, setLastPostFetched] = useState(0);
+  const [postCollectionSize, setPostCollectionSize] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const countPostFetchPerRequest = 2;
 
   useEffect(() => {
     async function getData() {
-      const fetchedData = await getPostsList();
-      setPostsList(fetchedData);
-      setPostsListUnchange(fetchedData);
+      const fetchedData = await getPostsList(
+        lastPostFetched,
+        countPostFetchPerRequest
+      );
+      setIsLoading(false);
+      if (postsList && postsListUnchange) {
+        setPostsList([...postsList, ...fetchedData]);
+        setPostsListUnchange([...postsListUnchange, ...fetchedData]);
+      } else {
+        setPostsList(fetchedData);
+        setPostsListUnchange(fetchedData);
+      }
+      const size = await getPostsCount();
+      console.log(size);
+      setPostCollectionSize(size);
     }
     getData();
-  }, []);
+  }, [lastPostFetched]);
 
   useEffect(() => {
-    console.log(mood, postsList, postsListUnchange);
     if (mood) {
       if (mood === "همه") setPostsList(postsListUnchange);
       else
@@ -35,25 +52,32 @@ const Blog = () => {
           postsListUnchange.filter((element) => element.category === mood)
         );
     }
+    console.log(mood);
   }, [mood]);
+
+  const getMorePost = () => {
+    setIsLoading(true);
+    const offset = lastPostFetched + countPostFetchPerRequest + 1;
+    setLastPostFetched(offset);
+  };
 
   return (
     <div className="cContainer">
       <MoodsList mood={setMood} />
-      <div className="homePostsContainer">
-        <div>
-          {!postsList ? (
-            <Loading showFullScreen={false} />
-          ) : (
-            postsList.map((el) => <Post key={el.image} data={el} />)
-          )}
-          {postsList && postsList.length === 0 && (
+      <div className="blogPostsContainer">
+        <div className="postsListContainer">
+          {postsList && postsList.map((el) => <Post key={el.id} data={el} />)}
+          {postsList.length === 0 && !isLoading && (
             <div className="postsListEmpty">
               <img src={listEmpty} />
               <p>{getText(key.HL_EMPTY_LIST)}</p>
             </div>
           )}
         </div>
+        {isLoading && <Loading showFullScreen={false} />}
+        {postsList.length < postCollectionSize && !isLoading && (
+          <button onClick={getMorePost}>مشاهده بیشتر</button>
+        )}
       </div>
     </div>
   );
--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,16 @@
 // Function
 import { TextKey, getText } from "./Text";
 // Firebase
-import { getDocs, collection, addDoc } from "firebase/firestore";
+import {
+  getDocs,
+  collection,
+  addDoc,
+  orderBy,
+  limit,
+  query,
+  startAt,
+  getCountFromServer,
+} from "firebase/firestore";
 import { db, fStorage } from "./firebase-config";
 import { ref, uploadBytes } from "firebase/storage";
 
@@ -11,9 +20,22 @@ const aboutMeCollectionRef = collection(db, "about_me");
 
 const key = new TextKey();
 
-const getPostsList = async () => {
-  const data = await getDocs(postsCollectionRef);
+const getPostsList = async (startId = 0, limitCount = 3) => {
+  const data = await getDocs(
+    query(
+      postsCollectionRef,
+      orderBy("id"),
+      limit(limitCount),
+      startAt(startId)
+    )
+  );
   return data.docs.map((doc) => ({ ...doc.data() }));
+};
+
+const getPostsCount = async () => {
+  const snapshot = await getCountFromServer(postsCollectionRef);
+  const size = snapshot.data().count;
+  return size;
 };
 
 const getMoodsList = async () => {
@@ -65,4 +87,4 @@ const getAboutMeData = async () => {
   return data.docs.map((doc) => ({ ...doc.data() }));
 };
 
-export { getPostsList, getMoodsList, sendPost, getAboutMeData };
+export { getPostsList, getPostsCount, getMoodsList, sendPost, getAboutMeData };

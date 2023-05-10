54a70f3767ea9d4a3e495f6ec1a09300dfc4330e
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-05-10 20:16:31 +0330

    Fix : English Lang Save To Local Storage
    Save Language State To Local Storage

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // styles & icons
-import style from "../sass/Navbar.scss";
-import gmailIcon from "../assets/images/gmail-icon.png";
-import arrowIcon from "../assets/images/arrowMenu.png";
-import languageIcon from "../assets/images/changeLanguage.svg";
+import style from '../sass/Navbar.scss';
+import gmailIcon from '../assets/images/gmail-icon.png';
+import arrowIcon from '../assets/images/arrowMenu.png';
+import languageIcon from '../assets/images/changeLanguage.svg';
 
 // function
-import { getText, TextKey } from "../Text";
-import { mSignUp, mSignOut } from "../requests";
-import { useTranslation } from "react-i18next";
-import i18n from "../i18n";
+import { getText, TextKey } from '../Text';
+import { mSignUp, mSignOut } from '../requests';
+import { useTranslation } from 'react-i18next';
+import i18n from '../i18n';
 
 const Navbar = () => {
   const key = new TextKey();
@@ -19,16 +19,18 @@ const Navbar = () => {
   const [isShowLogout, setIsShowLogout] = useState(false);
   const [userInfo, setUserInfo] = useState({
     isAuth: false,
-    id: "",
-    name: "",
-    email: "",
-    image: "",
+    id: '',
+    name: '',
+    email: '',
+    image: '',
   });
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
-    const userInfoLocalStorage = JSON.parse(localStorage.getItem("userInfo"));
+    const userInfoLocalStorage = JSON.parse(localStorage.getItem('userInfo'));
     userInfoLocalStorage && setUserInfo(userInfoLocalStorage);
+    // Get Current Lang From Local Storage And Set To Site
+    getLangFromLocalStorage() === 'en' && i18n.changeLanguage('en');
   }, []);
 
   const signInHandler = () => {
@@ -39,7 +41,7 @@ const Navbar = () => {
   const signOutHandler = () => {
     closeNavResponsive();
     mSignOut().then(() => {
-      setUserInfo({ isAuth: false, id: "", name: "", email: "", image: "" });
+      setUserInfo({ isAuth: false, id: '', name: '', email: '', image: '' });
       navHandlerResponsive();
     });
   };
@@ -53,48 +55,58 @@ const Navbar = () => {
   };
 
   const getOppositeLanguage = () => {
-    if (i18n.language === "fa") return "en";
-    else if (i18n.language === "en") return "fa";
+    if (i18n.language === 'fa') return 'en';
+    else if (i18n.language === 'en') return 'fa';
+  };
+
+  const saveLangToLocalStorage = (lang) => {
+    localStorage.setItem('LANGUAGE', lang);
+  };
+
+  const getLangFromLocalStorage = () => {
+    const data = localStorage.getItem('LANGUAGE');
+    return data;
   };
 
   return (
     <div>
       <header>
-        <Link to="/" onClick={closeNavResponsive}>
+        <Link to='/' onClick={closeNavResponsive}>
           <h1>{getText(key.NB_Main_Title, t, i18n)}</h1>
         </Link>
 
-        <div onClick={navHandlerResponsive} className="btnResponsiveMenu">
+        <div onClick={navHandlerResponsive} className='btnResponsiveMenu'>
           <span>{getText(key.NB_MenuResponsive, t, i18n)}</span>
           <img src={arrowIcon} />
         </div>
 
         <nav
           style={{
-            top: !navTopResponsive && "54px",
+            top: !navTopResponsive && '54px',
           }}
         >
           <ul>
             <li>
-              <Link to="/new-post" onClick={closeNavResponsive}>
+              <Link to='/new-post' onClick={closeNavResponsive}>
                 {getText(key.NB_MenuNewPost, t, i18n)}
               </Link>
             </li>
             <li>
-              <Link to="/blog" onClick={closeNavResponsive}>
+              <Link to='/blog' onClick={closeNavResponsive}>
                 {getText(key.NB_MenuBlogs, t, i18n)}
               </Link>
             </li>
             <li>
-              <Link to="/about-me" onClick={closeNavResponsive}>
+              <Link to='/about-me' onClick={closeNavResponsive}>
                 {getText(key.NB_AboutMe, t, i18n)}
               </Link>
             </li>
             <li>
               <span
-                className="changeLanguageSpan"
+                className='changeLanguageSpan'
                 onClick={() => {
                   i18n.changeLanguage(getOppositeLanguage());
+                  saveLangToLocalStorage(i18n.languages[0]);
                 }}
               >
                 <span>{getOppositeLanguage()}</span>
@@ -103,7 +115,7 @@ const Navbar = () => {
             </li>
             <li>
               {userInfo && userInfo.isAuth ? (
-                <div className="navbarUsername" onClick={signOutHandler}>
+                <div className='navbarUsername' onClick={signOutHandler}>
                   <div
                     onMouseEnter={() => setIsShowLogout(true)}
                     onMouseLeave={() => setIsShowLogout(false)}
@@ -112,7 +124,7 @@ const Navbar = () => {
                     <img src={userInfo.image} />
                   </div>
                   {isShowLogout && (
-                    <span className="showLogoutInfo">خروج از حساب</span>
+                    <span className='showLogoutInfo'>خروج از حساب</span>
                   )}
                 </div>
               ) : (
